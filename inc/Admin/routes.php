73ab12c19660c61af19a2418529cfbfea601b9f8
Nguyen Van Anh <anhskohbo@gmail.com>
2017-12-05 09:31:40 +0700

    Remove dummy code

--- inc/Admin/routes.php
+++ inc/Admin/routes.php
@@ -1,15 +1,5 @@
 <?php
 
-use AweBooking\Http\Router\Binding_Resolver;
-
-$route->get( '/', function () {
-	$binding = new Binding_Resolver( awebooking() );
-
-	$binding->bind( 'room_type', function() {
-
-	} );
-
-	dd($binding->resolve_bindings([ 'room_type' => 1 ]));
-
+$route->get( '/room/{room}', function ( $room_type ) {
 	return '~.~';
 });
