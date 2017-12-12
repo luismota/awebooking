e9873ca7422e1abe43571748d8145a8baa559bd5
HoangHuu <huuhoangcd221@gmail.com>
2017-12-12 15:00:50 +0700

    Temp Commit.

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,8 +21,8 @@ mix.js('assets/jssrc/admin/awebooking.js', 'assets/js/admin')
    .js('assets/jssrc/admin/manager-availability.js', 'assets/js/admin')
    .js('assets/jssrc/admin/calendar/schedule-calendar.js', 'assets/js/admin');
 
-mix.copy('./node_modules/sweetalert2/dist/sweetalert2.min.css', './assets/css/sweetalert2.min.css', false);
-mix.copy('./node_modules/sweetalert2/dist/sweetalert2.min.js', './assets/js/sweetalert2/sweetalert2.min.js', false);
+mix.copy('./node_modules/magnific-popup/dist/magnific-popup.css', './assets/css/magnific-popup.css', false);
+mix.copy('./node_modules/magnific-popup/dist/jquery.magnific-popup.min.js', './assets/js/magnific-popup/jquery.magnific-popup.min.js', false);
 mix.extract(['vue', 'form-serialize', 'popper.js', 'tooltip.js']);
 
 if (mix.inProduction()) {
